c5e04a690d53206efaab4ae3c1ae56912c3c14de
Josh Currier <doitaljosh@gmail.com>
2021-08-21 23:16:52 -0400

    Add adau1452 branch

---
+++ sigma-studio-files/adau1452-ad2410-tesla-amp/adau1452-ad2410-tesla-amp_IC_4_PARAM.h
@@ -0,0 +1,23 @@
+/*
+ * File:           C:\Users\doita\OneDrive\Documents\Analog Devices\SigmaStudio 4.6\Projects\adau1452-ad2410-tesla-amp\adau1452-ad2410-tesla-amp_IC_4_PARAM.h
+ *
+ * Created:        Saturday, August 21, 2021 10:56:28 PM
+ * Description:    adau1452-ad2410-tesla-amp:IC 4 parameter RAM definitions.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * This software may only be used to program products purchased from
+ * Analog Devices for incorporation by you into audio products that
+ * are intended for resale to audio product end users. This software
+ * may not be distributed whole or in any part to third parties.
+ *
+ * Copyright ©2021 Analog Devices, Inc. All rights reserved.
+ */
+#ifndef __ADAU1452_AD2410_TESLA_AMP_IC_4_PARAM_H__
+#define __ADAU1452_AD2410_TESLA_AMP_IC_4_PARAM_H__
+
+
+#endif
